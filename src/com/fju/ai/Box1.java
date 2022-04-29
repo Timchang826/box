fc225ec9cc5134834c9e63887606db13685489be
User <timchang0001@gmail.com>
2022-04-29 15:06:53 +0800

    intial commit 2

--- src/com/fju/ai/Box1.java
+++ src/com/fju/ai/Box1.java
@@ -2,9 +2,6 @@ package com.fju.ai;
 
 public class Box1 extends Box{
     public Box1(){
-        super(1,2,3);
-        height=10;
-        width=10;
-        height=10;
+        super(10,10,10);
     }
 }
